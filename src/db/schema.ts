ce2ad46b2841b0af735ef31cb9f82aec09bfa453
Manh Tin Nguyen <wavelet-kuestennah.0e@icloud.com>
2025-02-14 11:09:40 +0100

    chore: remove drizzle orm

--- src/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
-
-export const exampleTable = pgTable("example", {
-  id: uuid().defaultRandom().primaryKey(),
-  title: varchar({ length: 255 }).notNull(),
-  description: text(),
-});
